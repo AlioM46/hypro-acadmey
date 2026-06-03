b19f1679ee03c1be16720d686eeca33648a2185e
AlioM46 <aliomaralsloom@gmail.com>
2026-06-03 20:29:40 +0300

    refactor(logo): customize size support and hover transitions

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 interface LogoProps {
   className?: string;
+  imgClassName?: string;
   onClick?: () => void;
 }
 
-export default function Logo({ className = '', onClick }: LogoProps) {
+export default function Logo({ className = '', imgClassName = 'h-11 sm:h-14', onClick }: LogoProps) {
   return (
     <div onClick={onClick} className={`flex items-center select-none ${className}`}>
-      <img 
-        src="/logo.png" 
-        alt="HyPro Academy" 
-        className="h-11 sm:h-14 w-auto object-contain hover:opacity-95 transition-opacity" 
+      <img
+        src="/logo.png"
+        alt="HyPro Academy"
+        className={`${imgClassName} w-auto object-contain hover:opacity-95 transition-opacity`}
       />
     </div>
   );
